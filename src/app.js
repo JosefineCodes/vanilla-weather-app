2685affddf3c18871da912dc61f1c41ceeb98591
Phinni <93153106+Phinni@users.noreply.github.com>
2021-12-14 17:51:08 +0100

    removed console.log

--- src/app.js
+++ src/app.js
@@ -71,10 +71,9 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "49e735c03709a6e5740d57ba7f72eb96";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 
